982d13f62bbc5118aba1ebae5f35c8bcf4ad210b
tkjftkjf <tkjftkjf@user.github.com>
2025-04-03 03:13:40 +0900

    feat: add post list bloc

--- lib/bloc/post_list_bloc.dart
+++ lib/bloc/post_list_bloc.dart
@@ -1,3 +1,6 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_edu_first_task/api/api_client.dart';
+import 'package:flutter_edu_first_task/data/models/post_list_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,8 +11,8 @@ class PostListBloc extends Bloc<PostListEvent, PostListState> {
     on<_Load>((event, emit) async {
       try {
         emit(_Loading());
-        /*  */
-        emit(_Done());
+        PostListModel postlist = await ApiClient(Dio()).getPostList();
+        emit(_Done(postlist));
       } catch (e) {
         emit(_Error(e.toString()));
       }
@@ -27,6 +30,6 @@ sealed class PostListState with _$PostListState {
   const PostListState._();
   const factory PostListState.init() = _Initial;
   const factory PostListState.loading() = _Loading;
-  const factory PostListState.done() = _Done;
+  const factory PostListState.done(PostListModel postList) = _Done;
   const factory PostListState.error(String error) = _Error;
 }
