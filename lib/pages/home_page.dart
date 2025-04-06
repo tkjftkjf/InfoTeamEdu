f6eb3f7b3c52828abdbd1b6547b4ba5f275f88cc
tkjftkjf <tkjftkjf@user.github.com>
2025-04-06 13:46:12 +0900

    feat: Apply bloc to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,40 +1,69 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_edu_first_task/api/api_client.dart';
-import 'package:flutter_edu_first_task/data/models/post_list_model.dart';
-import 'package:flutter_edu_first_task/data/models/post_model.dart';
 import 'package:flutter_edu_first_task/widgets/post_widget.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
-  final ApiClient apiClient = ApiClient(Dio());
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_edu_first_task/bloc/post_list_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PostListBloc _postListBloc = PostListBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _postListBloc.add(const PostListEvent.load());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
-      child: FutureBuilder<PostListModel>(
-        future: apiClient.getPostList(),
-        builder: (context, snapshot) {
-          /* print(snapshot.error);
-          print(snapshot.data); */
-          /* 이 코드를 flutter web으로 빌드하면 오류남
-          DioException [connection error]: The connection errored: The XMLHttpRequest onError callback was called. This typically indicates an error on the network layer. This indicates an error which most likely cannot be solved by the library.
-           */
-          if (snapshot.hasData) {
-            PostListModel? postList = snapshot.data;
-            if (postList != null) {
-              List<PostModel> posts = postList.list;
-              return ListView.builder(
-                itemCount: postList.count,
-                itemBuilder: (context, index) {
-                  return PostWidget(post: posts[index]);
-                },
-              );
-            }
-          }
-          return CircularProgressIndicator();
+      child: RefreshIndicator(
+        onRefresh: () async {
+          _postListBloc.add(const PostListEvent.load());
         },
+        child: BlocBuilder<PostListBloc, PostListState>(
+          bloc: _postListBloc,
+          builder: (context, state) {
+            return state.map(
+              init: (_) => Text('빈화면입니다.'),
+              loading: (_) => Center(child: CircularProgressIndicator()),
+              done: (doneState) {
+                final posts = doneState.postList.list;
+                return ListView.builder(
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    return PostWidget(post: posts[index]);
+                  },
+                );
+              },
+              error:
+                  (errorState) => Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Error: ${errorState.error}'),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            context.read<PostListBloc>().add(
+                              const PostListEvent.load(),
+                            );
+                          },
+                          child: Text('Retry'),
+                        ),
+                      ],
+                    ),
+                  ),
+            );
+          },
+        ),
       ),
     );
   }
