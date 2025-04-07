e53c5f2007bdab22918a15fd4879a73c279bdf3b
tkjftkjf <tkjftkjf@user.github.com>
2025-04-08 02:54:08 +0900

    refactor: Simplify header widget adding hasButton parameter

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -3,34 +3,41 @@ import 'package:flutter_edu_first_task/gen/assets.gen.dart';
 import 'package:flutter_svg/svg.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
-  const Header({super.key, required this.title, this.isDepth = false});
+  const Header({
+    super.key,
+    required this.title,
+    this.isDepth = false,
+    this.hasButton = true,
+  });
   final String title;
   final bool isDepth;
+  final bool hasButton;
 
   @override
   Widget build(BuildContext context) {
-    if (isDepth) {
-      return AppBar(
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.arrow_back_ios_new_rounded),
-        ),
-        title: Text(title, style: TextStyle(fontWeight: FontWeight.w700)),
-        centerTitle: true,
-      );
-    } else {
-      return AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(title, style: TextStyle(fontWeight: FontWeight.w700)),
-        actions: [
-          GestureDetector(child: SvgPicture.asset(Assets.icons.search)),
-          SizedBox(width: 16),
-          GestureDetector(child: SvgPicture.asset(Assets.icons.write)),
-        ],
-      );
-    }
+    return AppBar(
+      leading:
+          isDepth
+              ? GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back_ios_new_rounded),
+              )
+              : null,
+      automaticallyImplyLeading: false,
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.w700)),
+      centerTitle: isDepth,
+      actions:
+          hasButton
+              ? [
+                GestureDetector(child: SvgPicture.asset(Assets.icons.search)),
+                SizedBox(width: 16),
+                GestureDetector(child: SvgPicture.asset(Assets.icons.write)),
+                SizedBox(width: 20),
+              ]
+              : null,
+    );
   }
 
   @override
